7b38599dd2078eaa9256ba4102a3daf18ddcddb7
Luca Cinnirella <luca.cinnirella@protonmail.com>
2026-05-27 03:35:41 +0200

    src/pycroxe/_connection.py: Reduced "Reflection" emphasis in docstring

--- src/pycroxe/_connection.py
+++ src/pycroxe/_connection.py
@@ -178,9 +178,9 @@ class CroXeConnection:
 
     def get_table_by_name(self, tablename: str) -> _Table:
         """
-        **Reflect** requested table into a :py:class`sqlalchemy.schema.Table`
-        object and return it. Tables that have been already **reflected** once
-        won't be **reflected** again: the method will just return the existing
+        *Reflect* requested table into a :py:class`sqlalchemy.schema.Table`
+        object and return it. Tables that have been already *reflected* once
+        won't be *reflected* again: the method will just return the existing
         :py:class`sqlalchemy.schema.Table` object.
 
         Parameters
